8389416ddbd6bf797c592c6da3791420d7107667
Álvaro <agonzalez1554@gmail.com>
2021-09-30 09:27:58 -0300

    apis_and_IntroOOP

--- apis/weather.rb
+++ apis/weather.rb
@@ -4,13 +4,12 @@ require 'json'
 
 # URI = Uniform Resource identifier 
 def weather_response
-  uri = URI("http://api.openweathermap.org/data/2.5/weather?q=Vi%C3%B1a%20del%20mar&appid=9484ee17fae08ece349011f24f3ccc6b")
-  Net::HTTP.start(uri.host, uri.port,
-    :use_ssl => uri.scheme == 'https') do |http|
-    request = Net::HTTP::Get.new uri
-    response = http.request request # Net::HTTPResponse object
-    JSON.parse(response.read_body) # Pasa el string que nos entrega la API a hashes.
-  end
+  url = URI("http://api.openweathermap.org/data/2.5/weather?q=Vi%C3%B1a%20del%20mar&appid=9484ee17fae08ece349011f24f3ccc6b")
+  # Crear un cliente http
+  http = Net::HTTP.new(url.host, url.port)
+  # Generar un solicitud
+  request = Net::HTTP::Get.new(url)
+  # Obtener la respuesta
+  response = http.request(request)
+  JSON.parse(response.read_body)
 end
-
-print weather_response
